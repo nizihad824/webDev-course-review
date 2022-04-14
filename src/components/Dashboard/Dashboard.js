bae39ab67a96dc78d6c0f3926f7bb478cb97ad00
nizihad824 <mdnurulislam97@gmail.com>
2022-04-15 01:18:23 +0200

    dashboard done

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 import React, { useEffect, useState } from 'react';
-import { Bar, BarChart, Tooltip, XAxis, YAxis } from 'recharts';
+import { Area, AreaChart, Bar, BarChart, CartesianGrid, Tooltip, XAxis, YAxis } from 'recharts';
 
 const Dashboard = () => {
     const [sells,setSells]=useState([])
@@ -10,12 +10,49 @@ const Dashboard = () => {
 
     },[])
     return (
-        <BarChart width={800} height={400} data={sells}>
+        <div style={{backgroundCOlor:'black' }}>
+           <div className='my-5 d-flex justify-content-center'>
+           <BarChart width={800} height={400} data={sells}>
             <Bar dataKey="revenue" fill="#8884d8" />
         <XAxis dataKey="investment"></XAxis>
         <Tooltip />
         <YAxis></YAxis>
         </BarChart>
+           </div>
+           {/* Area Chart */}
+
+        <div className='d-flex justify-content-center my-5'>
+        <AreaChart width={730} height={250} data={sells}
+  margin={{ top: 5, right: 10, left: 0, bottom: 0 }}>
+  <defs>
+    <linearGradient id="colorUv" x1="0" y1="0" x2="0" y2="1">
+      <stop offset="5%" stopColor="#8884d8" stopOpacity={0.8}/>
+      <stop offset="95%" stopColor="#8884d8" stopOpacity={0}/>
+    </linearGradient>
+    <linearGradient id="colorPv" x1="0" y1="0" x2="0" y2="1">
+      <stop offset="5%" stopColor="#82ca9d" stopOpacity={0.8}/>
+      <stop offset="95%" stopColor="#82ca9d" stopOpacity={0}/>
+    </linearGradient>
+  </defs>
+  <XAxis dataKey="name" />
+  <YAxis />
+  <CartesianGrid strokeDasharray="3 3" />
+  <Tooltip />
+  <Area type="monotone" dataKey="investment" stroke="#8884d8" fillOpacity={1} fill="url(#colorUv)" />
+  <Area type="monotone" dataKey="revenue" stroke="#82ca9d" fillOpacity={1} fill="url(#colorPv)" />
+  
+</AreaChart>
+        </div>
+       
+        
+        </div>
+       
+        
+        
+
+        
+
+
     );
 };
 
